7671ce7c17de5536bdb417857cda54a702ec5034
electricprogramming <yeungj316@gmail.com>
2024-10-06 19:25:59 -0400

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const utils = require('./utils/utils.json');
+
+console.log(utils.exampleKey); // Output: exampleValue
+console.log(utils.anotherKey);  // Output: 42
+
+// Add your additional logic to manipulate the DOM or other operations here
+document.getElementById('output').innerHTML = `
+    <p>Example Key: ${utils.exampleKey}</p>
+    <p>Another Key: ${utils.anotherKey}</p>
+`;
