8bd17a6178664420b5d8765f1371a5d8168b4635
electricprogramming <yeungj316@gmail.com>
2024-10-07 18:33:40 -0400

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,10 +1,6 @@
 import utils from './utils/utils.json'
-
-console.log(utils.exampleKey); // Output: exampleValue
-console.log(utils.anotherKey);  // Output: 42
-
-// Add your additional logic to manipulate the DOM or other operations here
-document.getElementById('output').innerHTML = `
-    <p>Example Key: ${utils.exampleKey}</p>
-    <p>Another Key: ${utils.anotherKey}</p>
-`;
+let output = ''
+utils.forEach(key => {
+  output += `${key}: ${utils.key}\n`
+})
+document.getElementById('output').innerHTML = output;
