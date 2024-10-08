2b7407b66c3f5532f5945da5fea606e11ee209d4
electricprogramming <yeungj316@gmail.com>
2024-10-08 14:32:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 import utils from './utils/utils.js'
 let output = ''
-utils.forEach(key => {
-  output += `${key}: ${utils.key}\n`
+Object.keys(utils).forEach(key => {
+  output += `${key}: ${utils[key]}\n`
 })
-document.getElementById('output').innerHTML = output;
+document.getElementById('output').innerHTML = `<pre>${output}</pre>`;
