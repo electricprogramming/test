bca69d713d7570ec315c622b90c9066926469f25
electricprogramming <yeungj316@gmail.com>
2024-10-08 15:57:48 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 import repeat from './utils/repeat.js';
 import chars from './utils/chars.js';
-import OysterError from './utils/OysterError.js
-throw new OysterError('Hello')
+import OysterError from './utils/OysterError.js;
+throw new OysterError('Hello');
