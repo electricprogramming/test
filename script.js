07926037c0a71811b13f965f46067c0ba6c2e80a
electricprogramming <yeungj316@gmail.com>
2024-10-08 15:50:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,2 @@
-import utils from './utils/utils.js'
-console.log(utils)
-let output = ''
-Object.keys(utils).forEach(key => {
-  output += `${key}: ${utils[key]}\n`
-})
-document.getElementById('output').innerHTML = `<pre>${output}</pre>`;
+import repeat from './utils/repeat.js';
+import chars from './utils/chars.js';
