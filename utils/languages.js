5e8950ea6898982681ce9d30b5829395ff983c59
electricprogramming <yeungj316@gmail.com>
2024-10-14 12:47:25 -0400

    Create languages.js

---
+++ utils/languages.js
@@ -0,0 +1,25 @@
+const languages = {
+    "English": "en",
+    "Mandarin Chinese": "zh",
+    "Spanish": "es",
+    "Hindi": "hi",
+    "Arabic": "ar",
+    "French": "fr",
+    "Bengali": "bn",
+    "Portuguese": "pt",
+    "Russian": "ru",
+    "German": "de"
+}
+const reverseLanguages = {
+    "en": "English",
+    "zh": "Mandarin Chinese",
+    "es": "Spanish",
+    "hi": "Hindi",
+    "ar": "Arabic",
+    "fr": "French",
+    "bn": "Bengali",
+    "pt": "Portuguese",
+    "ru": "Russian",
+    "de": "German"
+}
+export {languages,reverseLanguages}
