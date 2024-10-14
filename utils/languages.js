bca896e309418d52ae67c8cdf358abaa9d84d4d3
electricprogramming <yeungj316@gmail.com>
2024-10-14 16:39:50 -0400

    Update languages.js

--- utils/languages.js
+++ utils/languages.js
@@ -1,26 +1,26 @@
 const map1 = {
-    "English": "en",
-    "Chinese": "zh",
-    "Spanish": "es",
-    "Hindi": "hi",
-    "Arabic": "ar",
-    "French": "fr",
-    "Bengali": "bn",
-    "Portuguese": "pt",
-    "Russian": "ru",
-    "German": "de"
+  "English": "en",
+  "Chinese": "zh",
+  "Spanish": "es",
+  "Hindi": "hi",
+  "Arabic": "ar",
+  "French": "fr",
+  "Bengali": "bn",
+  "Portuguese": "pt",
+  "Russian": "ru",
+  "German": "de"
 }
 const map2 = {
-    "en": "English",
-    "zh": "Chinese",
-    "es": "Spanish",
-    "hi": "Hindi",
-    "ar": "Arabic",
-    "fr": "French",
-    "bn": "Bengali",
-    "pt": "Portuguese",
-    "ru": "Russian",
-    "de": "German"
+  "en": "English",
+  "zh": "Chinese",
+  "es": "Spanish",
+  "hi": "Hindi",
+  "ar": "Arabic",
+  "fr": "French",
+  "bn": "Bengali",
+  "pt": "Portuguese",
+  "ru": "Russian",
+  "de": "German"
 }
 const languages = {
   getCodeByName: function(name) {
@@ -31,4 +31,5 @@ const languages = {
     return map2[code];
   }
 };
+Object.deepFreeze(languages);
 export default languages;
