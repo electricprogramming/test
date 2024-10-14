96a7f598652196d712b8047452a98fd5f7faa180
electricprogramming <yeungj316@gmail.com>
2024-10-14 16:50:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,7 @@
 import repeat from './utils/repeat.js';
 import chars from './utils/chars.js';
 import OysterError from './utils/OysterError.js';
+import './utils/deepFreezeObj.js'
+Object.deepFreeze({})
 // Main portion of code
 // Do something
